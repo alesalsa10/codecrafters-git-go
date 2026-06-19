47216746eb48f7117529e3991939e747b79b96b1
alesalsa10 <alexandersalsa10@gmail.com>
2026-06-19 12:16:26 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -124,6 +124,68 @@ func main() {
 		}
 
 		fmt.Printf("%x\n", hash)
+	case "ls-tree":
+		//--name-only is Args[2]
+		hash := os.Args[3]
+		path := ".git/objects/" + hash[:2] + "/" + hash[2:]
+		//read file contents
+		uncompressedContent, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println(os.Stderr, "Error reading file: %s\n", err)
+			os.Exit(1)
+		}
+		//decompress file with zlib
+		reader, err := zlib.NewReader(bytes.NewReader(uncompressedContent))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
+			os.Exit(1)
+		}
+		//defer will close the reader before the function returns
+		defer reader.Close()
+		//tree <size>\0
+		//  <mode> <name>\0<20_byte_sha>
+		//  <mode> <name>\0<20_byte_sha>
+		decompressedBytes, err := io.ReadAll(reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
+			os.Exit(1)
+		}
+		//find null byte
+		nullByteIndex := bytes.IndexByte(decompressedBytes, 0)
+		if nullByteIndex == -1 {
+			fmt.Fprintf(os.Stderr, "Error finding null byte in decompressed file\n")
+			os.Exit(1)
+		}
+		//content is everything after the null byte
+		decompressedBytes = decompressedBytes[nullByteIndex+1:]
+		//format
+		//<mode><space><name>\0<20 raw bytes of sha>
+		//only need name
+		var nameBytes []byte
+
+		for i := 0; i < len(decompressedBytes); {
+			spaceIndex := bytes.IndexByte(decompressedBytes[i:], ' ')
+			if spaceIndex == -1 {
+				break
+			}
+			spaceIndex += i
+
+			nullByteIndex := bytes.IndexByte(decompressedBytes[spaceIndex+1:], 0)
+			if nullByteIndex == -1 {
+				fmt.Fprintf(os.Stderr, "Error finding null byte in decompressed file\n")
+				os.Exit(1)
+			}
+			nullByteIndex += spaceIndex + 1
+
+			nameBytes = append(nameBytes, decompressedBytes[spaceIndex+1:nullByteIndex]...)
+			nameBytes = append(nameBytes, '\n')
+
+			// Skip: name + null byte + 20 raw SHA bytes
+			i = nullByteIndex + 1 + 20
+		}
+
+		fmt.Print(string(nameBytes))
+
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
